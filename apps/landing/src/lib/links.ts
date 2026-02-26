2ed7ed9729d4561b81fddb99153c01b34a900a4f
Jack D <mrjackgd@gmail.com>
2026-02-26 17:30:38 +0000

    update hosted convex url

--- apps/landing/src/lib/links.ts
+++ apps/landing/src/lib/links.ts
@@ -4,4 +4,7 @@ export const OPENCOM_GITHUB_DOCS_URL = `${OPENCOM_GITHUB_REPO_URL}/tree/main/doc
 const OPENCOM_WEB_APP_URL =
   process.env.NEXT_PUBLIC_OPENCOM_WEB_APP_URL ?? "https://app.opencom.dev";
 
-export const OPENCOM_HOSTED_ONBOARDING_URL = `${OPENCOM_WEB_APP_URL.replace(/\/$/, "")}/?backendurl=wooden-moose-405.eu-west-1.convex.cloud&onboarding=hosted`;
+const HOSTED_BACKEND_URL =
+  process.env.NEXT_PUBLIC_CONVEX_URL ?? "https://outstanding-stork-558.eu-west-1.convex.cloud";
+
+export const OPENCOM_HOSTED_ONBOARDING_URL = `${OPENCOM_WEB_APP_URL.replace(/\/$/, "")}/?backendurl=${HOSTED_BACKEND_URL}&onboarding=hosted`;
